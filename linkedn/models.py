a9640528795fff77c820593e33873cedd290af63
root <root@debian>
2015-01-27 15:15:23 +0000

    Final models

--- linkedn/models.py
+++ linkedn/models.py
@@ -1,14 +1,35 @@
 from django.db import models
-
-class User(models.Model):
-
-    user =  models.CharField(max_length=20)
-    oauth_token_secret =  models.CharField(max_length=200)
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
-# Create your models here.
-    user = models.ForeignKey(User)
-    auth_token = models.CharField(max_length=200)
-    auth_secret = models.CharField(max_length=200)
+
+    user = models.ManyToManyField(User)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
 
+class Education(models.Model):
+
+    user = models.ManyToManyField(User)
+    school_name =  models.CharField(max_length=20) 
+    field_study =  models.CharField(max_length=20)
+    degree =  models.CharField(max_length=20)
+    start_date =  models.CharField(max_length=20)
+    end_date =  models.CharField(max_length=20)
+
+class Positions(models.Model):
+
+    user = models.ManyToManyField(User)
+    title =  models.CharField(max_length=20)
+    company =  models.CharField(max_length=20)
+    summary =  models.TextField()
+    is_current =  models.SmallIntegerField()
+    start_date_year =  models.CharField(max_length=20)
+    start_date_month =  models.CharField(max_length=20)
+    end_date_year=  models.CharField(max_length=20)
+    end_date_month =  models.CharField(max_length=20)
+
+class Certifications(models.Model):
+
+    user = models.ManyToManyField(User)
+    name = models.CharField(max_length=20)
