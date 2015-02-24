5ee21a4fc95f0ac8fd7ce5cddee4c854155ed69c
Javier Caro <root@debian>
2015-02-24 13:05:01 +0000

    Models finished

--- linkedn/models.py
+++ linkedn/models.py
@@ -3,33 +3,44 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 
-    user = models.ManyToManyField(User)
+    user = models.ForeignKey(User)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.first_name, self.last_name
 
 class Education(models.Model):
 
-    user = models.ManyToManyField(User)
-    school_name =  models.CharField(max_length=20) 
-    field_study =  models.CharField(max_length=20)
-    degree =  models.CharField(max_length=20)
+    user = models.ForeignKey(User)
+    school_name =  models.CharField(max_length=100) 
+    field_study =  models.CharField(max_length=100)
+    degree =  models.CharField(max_length=100)
     start_date =  models.CharField(max_length=20)
     end_date =  models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.field_study
+
 class Positions(models.Model):
 
-    user = models.ManyToManyField(User)
-    title =  models.CharField(max_length=20)
-    company =  models.CharField(max_length=20)
+    user = models.ForeignKey(User)
+    title =  models.CharField(max_length=100)
+    company =  models.CharField(max_length=100)
     summary =  models.TextField()
-    is_current =  models.SmallIntegerField()
+    is_current =  models.CharField(max_length=20)
     start_date_year =  models.CharField(max_length=20)
     start_date_month =  models.CharField(max_length=20)
     end_date_year=  models.CharField(max_length=20)
     end_date_month =  models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.title
+
 class Certifications(models.Model):
 
-    user = models.ManyToManyField(User)
-    name = models.CharField(max_length=20)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
